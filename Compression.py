f05b0007e0d7b3e905b8e9b81f9659fafc9fb24d
marwa <marwasan03@gmail.com>
2025-11-20 19:21:52 +0100

    fixed holes pour de vrai

--- Compression.py
+++ Compression.py
@@ -9,16 +9,35 @@ def edge_length(v1, v2):
     return np.linalg.norm(v1.array - v2.array)
 
 
-def is_collapse_valid(v1, v2, collapsed_vertices):
-    if v1 in collapsed_vertices or v2 in collapsed_vertices:
+def is_collapse_valid(v1, v2, collapsed_vertices, vertices):
+    # Condition 1
+    if v1.id in collapsed_vertices or v2.id in collapsed_vertices:
         return False
+    
+    # Condition 2
     v1neighbors = v1.get_neighbors()
     v2neighbors = v2.get_neighbors()
     common_neighbors = set(v1neighbors).intersection(set(v2neighbors))
     if len(common_neighbors) != 2:
         return False
+    
+    id_w1, id_w2 = common_neighbors
+    w1 = vertices[id_w1]
+    w2 = vertices[id_w2]
+
+    # Condition 2.2
+    if id_w1 in w2.get_neighbors() or id_w2 in w1.get_neighbors():
+        return False
+
+    fw1 = Face(v1.id, v2.id, id_w1, -1)
+    fw2 = Face(v1.id, v2.id, id_w2,-2)
+    if not(fw1 in v1.faces and fw1 in v2.faces and fw2 in v1.faces and fw2 in v2.faces):
+        return False
+    
+    # Condition 3
     if any(v in collapsed_vertices for v in (v1neighbors + v2neighbors)):
         return False
+
     return True
 
 
@@ -35,17 +54,17 @@ def select_edge_collapses(vertices, nb_collapses):
 
     edges_with_cost = [(e, edge_length(*e)) for e in edges]
     edges_with_cost.sort(key=lambda x: x[1])
-
+    
     selected = []
     collapsed_vertices = set()
 
     for (v1, v2), _ in edges_with_cost:
-        if is_collapse_valid(v1, v2, collapsed_vertices):
+        if is_collapse_valid(v1, v2, collapsed_vertices, vertices):
             selected.append((v1, v2))
             collapsed_vertices.update([v1.id, v2.id])
         if len(selected) >= nb_collapses:
             break
-        
+    
 
     return selected
 
@@ -116,6 +135,7 @@ def squeeze_compression(object3D, compression_ratio=0.1):
                               "cut_ids": cut_ids,
                               "ordered_neighbors": ordered_neighbors
                               })
+
 
     return new_obj, collapse_info
 
